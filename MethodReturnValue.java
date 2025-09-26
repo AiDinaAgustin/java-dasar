d8cded45b01b3afdfd1ffa30ad70c341e89be712
AiDinaAgustin <aidinaagustin2@gmail.com>
2025-09-26 14:16:45 +0700

    Add hitung method to perform basic arithmetic operations

--- MethodReturnValue.java
+++ MethodReturnValue.java
@@ -2,10 +2,23 @@ public class MethodReturnValue {
     public static void main(String[] args) {
         var result = add(10, 20);
         System.out.println("Sum is: " + result);
+
+        System.out.println(hitung(100, "*", 200));
+
     }
 
     static int add(int a, int b) {
         var sum = a + b;
         return sum;
+    }
+
+    static int hitung(int value1, String operator, int value2) {
+        return switch (operator) {
+            case "+" -> value1 + value2;
+            case "-" -> value1 - value2;
+            case "*" -> value1 * value2;
+            case "/" -> value1 / value2;
+            default -> 0;
+        };
     }
 }
