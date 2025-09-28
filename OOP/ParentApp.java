45999b06ebed45504f8ebd56f0ac4a80ac3ca348
AiDinaAgustin <aidinaagustin2@gmail.com>
2025-09-28 18:16:53 +0700

    Add Parent and Child classes with method overriding for demonstration

---
+++ OOP/ParentApp.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class ParentApp {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.name = "Dina";
+        child.doIt();
+        System.out.println(child.name);
+
+        Parent parent = (Parent) child;
+        parent.name = "Rere";
+        parent.doIt();
+        System.out.println(parent.name);
+    }
+}
